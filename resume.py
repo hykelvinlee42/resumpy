77776b02fa48a13e9497876e922a43b35daa2308
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2022-11-16 12:45:11 -0500

    renewed resume directory setup in script

--- resume.py
+++ resume.py
@@ -2,10 +2,11 @@ from pylatex import Document, Command
 from resume_sections import heading, overview, work, experience, education
 from resume_setups import packages as pkgs, colors, docsetup
 import json
+import os
 
 
 def fill_document(doc):
-    resume_file = open("./resume.json")
+    resume_file = open("./import/resume.json")
     resume_data = json.load(resume_file)
     heading.add_heading(doc, resume_data)
     overview.add_overview(doc, resume_data)
@@ -26,6 +27,9 @@ def build_resume(debug, filename="resume"):
     doc.generate_tex()
     doc.generate_pdf(clean=False, clean_tex=False, compiler="lualatex")
     doc.generate_pdf(clean=(not debug), clean_tex=False, compiler="lualatex")  # compile twice in order for transparent package to work, reference: https://tex.stackexchange.com/questions/297294/pdflatex-transparent-package-seems-not-to-work
+    # move files to export directory
+    os.rename("./{0}.pdf".format(filename), "./export/{0}.pdf".format(filename))
+    os.rename("./{0}.tex".format(filename), "./export/{0}.tex".format(filename))
 
 
 if __name__ == "__main__":
