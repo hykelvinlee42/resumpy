d2d2c8851909dd13f99122e526c422ff417399d0
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2022-11-16 12:51:31 -0500

    renewed cover letter directory setup in script

--- letter.py
+++ letter.py
@@ -4,15 +4,16 @@ from resume_setups import packages as pkgs, colors, docsetup
 import datetime
 import json
 import sys
+import os
 
 
 def fill_document(doc):
-    resume_file = open("./resume.json")
+    resume_file = open("./import/resume.json")
     resume_data = json.load(resume_file)
     heading.add_heading(doc, resume_data, doc_type="letter")
     date = datetime.datetime.now()
     recipient = sys.argv[1] if len(sys.argv) > 1 else "Hiring Manager"
-    letter_data = open("./letter.txt")
+    letter_data = open("./import/letter.txt")
     lettercontent.add_content(doc, date, recipient, letter_data, resume_data)
 
 
@@ -28,6 +29,9 @@ def build_letter(debug, filename="cover-letter"):
     doc.generate_tex()
     doc.generate_pdf(clean=False, clean_tex=False, compiler="lualatex")
     doc.generate_pdf(clean=(not debug), clean_tex=False, compiler="lualatex")  # compile twice in order for transparent package to work, reference: https://tex.stackexchange.com/questions/297294/pdflatex-transparent-package-seems-not-to-work
+    # move files to export directory
+    os.rename("./{0}.pdf".format(filename), "./export/{0}.pdf".format(filename))
+    os.rename("./{0}.tex".format(filename), "./export/{0}.tex".format(filename))
 
 
 if __name__ == "__main__":
