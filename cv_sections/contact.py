02156f96b7a3979b5ecaca85663392b17967f4fb
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2024-07-29 22:54:05 -0400

    implemented support for multiple work emails in cv export

--- cv_sections/contact.py
+++ cv_sections/contact.py
@@ -4,13 +4,17 @@ from pylatex import Command, NoEscape, Section
 def add_contact(doc, cv_data):
     with doc.create(Section("Contact")):
         doc.append(Command("textbf", arguments=NoEscape("Work E-mail" + " ")))
-        doc.append(
-            Command(
-                "href",
-                arguments=NoEscape("mailto:" + cv_data["contact"]["work_email"]),
-                extra_arguments=cv_data["contact"]["work_email"],
+        for index, email in enumerate(cv_data["contact"]["work_email"]):
+            doc.append(
+                Command(
+                    "href",
+                    arguments=NoEscape("mailto:" + email),
+                    extra_arguments=email,
+                )
             )
-        )
+            if index != len(cv_data["contact"]["work_email"]) - 1:
+                doc.append(", ")
+
         doc.append(NoEscape("\n\\vspace{0.1cm}\n\n"))
 
         doc.append(Command("textbf", arguments=NoEscape("Personal E-mail" + " ")))
