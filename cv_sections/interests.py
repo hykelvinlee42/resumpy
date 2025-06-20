fc8e0ab64fc57c56b98f3f4c0d78db9e7a6f7dfd
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2025-06-20 16:07:18 -0400

    updated resumpy to use quarto templates

--- cv_sections/interests.py
+++
@@ -1,6 +0,0 @@
-from pylatex import NoEscape, Section
-
-
-def add_research_interests(doc, cv_data):
-    with doc.create(Section("Research Interests")):
-        doc.append(NoEscape(", ".join(cv_data["interests"])))
--- cv_setups/colors.py
+++
@@ -1,6 +0,0 @@
-colors = {"DarkBlue": ["rgb", "0.0, 0.0, 0.3"]}
-
-
-def add_colors(doc):
-    for color in colors.keys():
-        doc.add_color(color, model=colors[color][0], description=colors[color][1])
